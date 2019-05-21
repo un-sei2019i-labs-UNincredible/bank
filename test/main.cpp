bd72ac4bfd986f5905a580778c5d84361b1c23c3
HASPIMA <hpintom@unal.edu.co>
2019-05-21 17:40:25 -0500

    Combinatory function was made.
    The software allows input and output via std::cin and std::cout

--- test/main.cpp
+++ test/main.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 
 int division(int a, int b)
 {
@@ -18,7 +19,22 @@ int factorial(int n)
     return n * factorial(n - 1);
 }
 
+int combinatory(int n, int x)
+{
+    return factorial(n) / (factorial(x) * factorial(substraction(n, x)));
+}
+
 int main(int argc, char const *argv[])
 {
+    int n, x;
+
+    std::cout << "Please type n: ";
+    std::cin >> n;
+
+    std::cout << "Please type x: ";
+    std::cin >> x;
+
+    std::cout << "The combinatory(n,x) = " << combinatory(n, x) << std::endl;
+
     return 0;
 }
