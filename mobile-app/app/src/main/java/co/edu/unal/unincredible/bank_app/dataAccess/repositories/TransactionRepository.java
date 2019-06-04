2a364b22c55668ab4da0d1f27eb7e5fd0d98ba77
abihido <jcristanchor@unal.edu.co>
2019-06-04 17:09:18 -0500

    TransactionRepository was created.

---
+++ mobile-app/app/src/main/java/co/edu/unal/unincredible/bank_app/dataAccess/repositories/TransactionRepository.java
@@ -0,0 +1,16 @@
+package co.edu.unal.unincredible.bank_app.dataAccess.repositories;
+
+import co.edu.unal.unincredible.bank_app.dataAccess.models.TransactionDao;
+
+public class TransactionRepository {
+
+	private TransactionDao transactionDao;
+
+	/*
+	 * TODO:
+	 *
+	 * createTransaction()
+	 * */
+
+
+}
