c4e4ae7bd89771f0cd768a14746d2d2020b8a05d
HASPIMA <hpintom@unal.edu.co>
2019-06-04 15:59:33 -0500

    AccountRepository can create accounts.

--- mobile-app/app/src/main/java/co/edu/unal/unincredible/bank_app/dataAccess/repositories/AccountRepository.java
+++ mobile-app/app/src/main/java/co/edu/unal/unincredible/bank_app/dataAccess/repositories/AccountRepository.java
@@ -1,17 +1,30 @@
 package co.edu.unal.unincredible.bank_app.dataAccess.repositories;
 
+import co.edu.unal.unincredible.bank_app.dataAccess.models.Account;
+import co.edu.unal.unincredible.bank_app.dataAccess.models.AccountDao;
+import co.edu.unal.unincredible.bank_app.dataAccess.models.DaoSession;
+
 public class AccountRepository {
+
+	private AccountDao accountDao;
+
 	/*
 	* TODO:
 	*
-	* createAccount()
 	* getAccountById()
 	* updateAccount()
 	* deleteAccount()
 	*
 	* */
 
-	public void createAccount(String name, String password){
+	public AccountRepository(DaoSession session) {
+		this.accountDao = session.getAccountDao();
+	}
+
+	public Account createAccount(String uid, int amount) {
+		Account account = new Account(uid, amount);
+		this.accountDao.insert(account);
+		return account;
 
 	}
 
