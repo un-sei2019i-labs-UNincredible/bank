93825ecf4b04e4c8a44de4949242de270a1a5662
draco1006 <mbarragani@unal.edu.co>
2019-06-03 20:15:30 -0500

    Transaction ahora implementa el orm de greendao

--- mobile-app/app/src/main/java/co/edu/unal/unincredible/bank_app/dataAccess/models/Transaction.java
+++ mobile-app/app/src/main/java/co/edu/unal/unincredible/bank_app/dataAccess/models/Transaction.java
@@ -1,33 +1,74 @@
 package co.edu.unal.unincredible.bank_app.dataAccess.models;
 
+import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.NotNull;
+import org.greenrobot.greendao.annotation.Property;
+
 import java.util.Date;
 
-public class Transaction {
-	private String receiver;
-	private  String source;
-	private int amount;
-	private Date date;
+import org.greenrobot.greendao.annotation.Generated;
 
-	public Transaction(String receiver, String source, int amount, Date date) {
+@Entity
+public class Transaction {
+	@NotNull
+	@Property(nameInDb = "receiver")
+	private String receiver;
+	
+	@NotNull
+	@Property(nameInDb = "source")
+	private String source;
+	
+	@NotNull
+	@Property(nameInDb = "amount")
+	private int amount;
+	
+	@NotNull
+	@Property(nameInDb = "date")
+	private Date date;
+	
+	@Generated(hash = 468412022)
+	public Transaction(@NotNull String receiver, @NotNull String source, int amount,
+					   @NotNull Date date) {
 		this.receiver = receiver;
 		this.source = source;
 		this.amount = amount;
 		this.date = date;
 	}
-
+	
+	@Generated(hash = 750986268)
+	public Transaction() {
+	}
+	
 	public String getReceiver() {
-		return receiver;
+		return this.receiver;
 	}
-
+	
+	public void setReceiver(String receiver) {
+		this.receiver = receiver;
+	}
+	
 	public String getSource() {
-		return source;
+		return this.source;
 	}
-
+	
+	public void setSource(String source) {
+		this.source = source;
+	}
+	
 	public int getAmount() {
-		return amount;
+		return this.amount;
 	}
-
+	
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	
 	public Date getDate() {
-		return date;
+		return this.date;
 	}
+	
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
 }
