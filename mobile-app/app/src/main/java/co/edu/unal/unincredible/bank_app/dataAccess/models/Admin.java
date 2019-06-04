684fda355fa24458176e55850eb9fd6ca8f86ddd
HASPIMA <hpintom@unal.edu.co>
2019-06-03 19:33:41 -0500

    GrenDAO was imported to the project.
    Admin class was deleted because of the E-R diagram consistency.
    
    Co-Authored-By: null <abihido@users.noreply.github.com>
    Co-Authored-By: null <draco1006@users.noreply.github.com>

--- mobile-app/app/src/main/java/co/edu/unal/unincredible/bank_app/dataAccess/models/Admin.java
+++
@@ -1,9 +0,0 @@
-package co.edu.unal.unincredible.bank_app.dataAccess.models;
-
-public class Admin extends User {
-	public Admin(String uid) {
-		super(uid);
-	}
-
-	//TODO: Create methods of admin account
-}
