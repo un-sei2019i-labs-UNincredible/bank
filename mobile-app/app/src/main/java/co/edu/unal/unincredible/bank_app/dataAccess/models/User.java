d9ff3671ab7c4945adff3d9d298ea2f1cfd5872e
abihido <jcristanchor@unal.edu.co>
2019-06-03 20:46:24 -0500

    User implement GreenDAO ORM.

--- mobile-app/app/src/main/java/co/edu/unal/unincredible/bank_app/dataAccess/models/User.java
+++ mobile-app/app/src/main/java/co/edu/unal/unincredible/bank_app/dataAccess/models/User.java
@@ -1,50 +1,22 @@
 package co.edu.unal.unincredible.bank_app.dataAccess.models;
 
+import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Id;
+import org.greenrobot.greendao.annotation.NotNull;
+import org.greenrobot.greendao.annotation.Property;
+
+@Entity
 public class User {
+	
+	@Property(nameInDb = "uid")
+	@Id
 	private String uid;
+	
+	@Property(nameInDb = "account")
 	private Account account;
+	
+	@Property(nameInDb = "name")
+	@NotNull
 	private String displayName;
-
-	public String getUid() {
-		return uid;
-	}
-
-	public void setUid(String uid) {
-		this.uid = uid;
-	}
-
-	public Account getAccount() {
-		return account;
-	}
-
-	public void setAccount(Account account) {
-		this.account = account;
-	}
-
-	public User(String uid) {
-		this.uid = uid;
-	}
-
-	public User(String uid, Account account) {
-		this.uid = uid;
-		this.account = account;
-	}
-
-	public User(String uid, Account account, String displayName) {
-		this.uid = uid;
-		this.account = account;
-		this.displayName = displayName;
-	}
-
-	public String getDisplayName() {
-		return displayName;
-	}
-
-	public void setDisplayName(String displayName) {
-		this.displayName = displayName;
-	}
-
-	public void transfer(String receiver, int amount) {
-		//TODO: transfer to another user
-	}
+	
 }
