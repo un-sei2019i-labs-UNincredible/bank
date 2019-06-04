ec78450a8fa371aff8bbde34a7a42a6a499b63bc
draco1006 <mbarragani@unal.edu.co>
2019-06-03 21:19:05 -0500

    User now implements GreenDAO methods

--- mobile-app/app/src/main/java/co/edu/unal/unincredible/bank_app/dataAccess/models/User.java
+++ mobile-app/app/src/main/java/co/edu/unal/unincredible/bank_app/dataAccess/models/User.java
@@ -4,6 +4,7 @@ import org.greenrobot.greendao.annotation.Entity;
 import org.greenrobot.greendao.annotation.Id;
 import org.greenrobot.greendao.annotation.NotNull;
 import org.greenrobot.greendao.annotation.Property;
+import org.greenrobot.greendao.annotation.Generated;
 
 @Entity
 public class User {
@@ -15,5 +16,31 @@ public class User {
 	@Property(nameInDb = "name")
 	@NotNull
 	private String displayName;
+
+	@Generated(hash = 1879096950)
+	public User(String uid, @NotNull String displayName) {
+		this.uid = uid;
+		this.displayName = displayName;
+	}
+
+	@Generated(hash = 586692638)
+	public User() {
+	}
+
+	public String getUid() {
+		return this.uid;
+	}
+
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+
+	public String getDisplayName() {
+		return this.displayName;
+	}
+
+	public void setDisplayName(String displayName) {
+		this.displayName = displayName;
+	}
 	
 }
