a03dc522d5ef51f9f252e5af669bb2620c52d400
HASPIMA <hpintom@unal.edu.co>
2019-06-04 19:32:14 -0500

    TransactionRepository was made. Preparing for finish up this feature.

--- mobile-app/app/src/main/java/co/edu/unal/unincredible/bank_app/dataAccess/repositories/TransactionRepository.java
+++ mobile-app/app/src/main/java/co/edu/unal/unincredible/bank_app/dataAccess/repositories/TransactionRepository.java
@@ -1,21 +1,32 @@
 package co.edu.unal.unincredible.bank_app.dataAccess.repositories;
 
+import java.util.Date;
+
+import co.edu.unal.unincredible.bank_app.dataAccess.models.Account;
 import co.edu.unal.unincredible.bank_app.dataAccess.models.DaoSession;
+import co.edu.unal.unincredible.bank_app.dataAccess.models.Transaction;
 import co.edu.unal.unincredible.bank_app.dataAccess.models.TransactionDao;
 
 public class TransactionRepository {
 
 	private TransactionDao transactionDao;
 
-	/*
-	 * TODO:
-	 *
-	 * createTransaction()
-	 * */
-
-	public TransactionRepository(DaoSession session){
+	public TransactionRepository(DaoSession session) {
 		this.transactionDao = session.getTransactionDao();
 	}
 
+	public Transaction createTransaction(Account source, Account receiver, int amount) {
+		Date now = new Date();
 
+		//source transaction
+		Transaction srcTransaction = new Transaction(receiver.getUid(), source.getUid(), amount, now);
+
+		//receiver transaction
+		Transaction rcvTransaction = new Transaction(source.getUid(), receiver.getUid(), amount, now);
+
+		this.transactionDao.insert(rcvTransaction);
+		this.transactionDao.insert(srcTransaction);
+
+		return srcTransaction;
+	}
 }
