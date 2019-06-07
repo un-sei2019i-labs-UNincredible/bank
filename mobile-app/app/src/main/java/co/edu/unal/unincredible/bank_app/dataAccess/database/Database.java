f0ee638e46f9c31eac093733adad72b446a01041
HASPIMA <hpintom@unal.edu.co>
2019-06-07 17:13:02 -0500

    Now you can get a session when you need to access to the database.

--- mobile-app/app/src/main/java/co/edu/unal/unincredible/bank_app/dataAccess/database/Database.java
+++ mobile-app/app/src/main/java/co/edu/unal/unincredible/bank_app/dataAccess/database/Database.java
@@ -1,8 +1,10 @@
 package co.edu.unal.unincredible.bank_app.dataAccess.database;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 
 import co.edu.unal.unincredible.bank_app.dataAccess.models.DaoMaster;
+import co.edu.unal.unincredible.bank_app.dataAccess.models.DaoSession;
 
 public class Database extends DaoMaster.OpenHelper {
 
@@ -14,6 +16,16 @@ public class Database extends DaoMaster.OpenHelper {
 	public void onCreate(org.greenrobot.greendao.database.Database db) {
 		super.onCreate(db);
 		db.execSQL("insert into USER(uid,name,admin)values ('user','123456',0)");
+	}
+
+	public DaoSession getNewDaoSesion(Context context) {
+		Database helper = new Database(context);
+		SQLiteDatabase db = helper.getWritableDatabase();
+		DaoMaster daoMaster = new DaoMaster(db);
+		DaoSession daoSession = daoMaster.newSession();
+
+		return daoSession;
+
 	}
 }
 
