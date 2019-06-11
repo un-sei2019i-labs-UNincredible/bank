daba7d003ac9b226f28b03a7aa50570c2ff08700
draco1006 <mbarragani@unal.edu.co>
2019-06-10 23:43:13 -0500

    SendMoneyActivity was created.

---
+++ mobile-app/app/src/main/java/co/edu/unal/unincredible/bank_app/presentation/activities/SendMoneyActivity.java
@@ -0,0 +1,17 @@
+package co.edu.unal.unincredible.bank_app.presentation.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import co.edu.unal.unincredible.bank_app.R;
+
+public class SendMoneyActivity extends AppCompatActivity {
+
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.send_money_layout);
+	}
+}
